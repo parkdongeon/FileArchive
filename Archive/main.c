92f072555ea94610aaa120f2d6ac0427ac529ee6
Dongeon Park <dongun8943@gmail.com>
2018-11-29 16:33:41 +0900

    Function getFileSize and main.

--- Archive/main.c
+++ Archive/main.c
@@ -29,3 +29,35 @@ typedef struct _ARCHIVE {
 }ARCHIVE, *PARCHIVE;
 
 #define ARCHIVE_NAME "archive.bin"
+
+//파일의 크기를 구하는 함수
+uint32_t getFileSize(FILE *fp)
+{
+	uint32_t size;
+	uint32_T currPos = ftell(fp);		//파일 포인터의 위치 저장
+
+	fsekk(fp, 0, SEEK_END);
+	size = ftell(fp);
+
+	fseek(fp, currPos, SEEK_SET);
+
+	return size;
+}
+
+int main()
+{
+	PARCHIVE archive =malloc(sizeof(ARCHIVE));
+	memset(archive, 0, sizeof(ARCHIVE));
+
+	FILE *fp = fopen(ARCHIVE_NAME, "r+b");		//아카이브 파일을 읽기모드/쓰기모드로 열기
+	if (fp == NULL)									//아카이브 파일이 없으면
+	{
+		fp = open(ARCHIVE_NAME, "w+b");			//아카이브 파일 생성
+		if (fp == NULL)								//파일 생성에 실패하면
+			return -1;									//프로그램 종료
+	}
+	return 0;
+}
+
+
+
