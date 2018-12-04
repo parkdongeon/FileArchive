9647d1a3d151f314fa7168f4bd8b58c866320d0e
Dongeon Park <dongun8943@gmail.com>
2018-12-05 00:40:24 +0900

    add function:append

--- Archive/main.c
+++ Archive/main.c
@@ -44,6 +44,71 @@ uint32_t getFileSize(FILE *fp)
 	return size;
 }
 
+
+// 파일 추가 함수
+int append(PARCHIVE archive, char *filename)
+{
+	int ret = 0; // 함수가 성공했는지 실패했는지 반환
+
+	FILE *fp = fopen(filename, "rb");
+	if (fp == NULL)
+	{
+		printf("%s 파일이 없습니다.\n", filename);
+		return -1; // 함수 종료. 실패
+	}
+
+
+	uint8_t *buffer;
+	uint32_t size;
+
+	size = getFIleSize(fp); // 추가할 파일의 크기
+	buffer = malloc(size);
+
+	// 추가할 파일의 내용 읽음
+	if (fread(buffer, size, 1, fp) < 1)
+	{
+		printf("%s 파일 읽기 실패\n", filename);
+		ret = -1;	// -1은 실패\n
+		goto Error1;	// fp를 닫고 buffer를 해제하는 에러 처리로 이동
+	}
+
+	//새 파일 정보 생성
+	PFILE_DESC desc = malloc(sizeof(FILE_DESC));
+	memset(desc, 0, sizeof(FILE_DESC));
+	strcpy(desc->name, filename);
+	desc->size = size;
+
+	fseek(archive->fp, sizeof(ARCHIVE_HEADER), SEEK_SET);
+
+	desc->dataOffset = ftell(archive->fp) + sizeof(FILE_DESC);
+
+	if(fwrite(desc, sizeof(FILE_DESC), 1, archive->fp) < 1)
+	{
+		printf("파일 정보 쓰기 실패\n");
+		ret = -1;
+		goto Error2;
+	}
+
+	if(fwrite(buffer, size, 1, archive->fp) < 1)
+	{
+		printf("파일 데이터 쓰기 실패\n");
+		ret == -1;
+		goto Error2;
+	}
+
+	printf("%s" 파일 추가 성공\n크기 : %d\n", filename, size);
+}
+
+Error2:
+	free(desc);
+
+Error1:
+	free(buffer);
+	fclose(fp);
+
+	return ret;
+}
+
 int main()
 {
 	PARCHIVE archive =malloc(sizeof(ARCHIVE));
