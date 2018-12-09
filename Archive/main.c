d2c658e26c1beeb8a9904818da30b31a0adf6a96
Dongeon Park <dongun8943@gmail.com>
2018-12-10 07:20:12 +0900

    add function:extract

--- Archive/main.c
+++ Archive/main.c
@@ -126,6 +126,63 @@ void list(PARCHIVE archive)
 		curr = curr->next;
 	
 }
+
+int extract(PARCHIVE archive, char *filename)
+{
+	PFILE_NODE curr = archive->fileList.next;
+
+	while (curr != NULL)
+	{
+
+		if (strcmp(curr->desc.name, filename) == 0)
+		{
+			int ret = 0;
+			uint32_t size = curr->desc.size;
+			uint8_t *buffer = malloc(size);
+
+			fseek(archive->fp, curr->desc.dataOffset, SEEK_SET);
+
+			if (fread(buffer, size, 1, archive->fp) < 1)
+			{
+				printf("아카이브 파일 읽기 실패\n");
+				ret = -1;
+				goto Error1;
+			}
+
+			FILE *fp = fopen(filename, "wb");
+
+			if (fp == NULL)
+			{
+				printf("%s 파일 열기 실패\n", filename);
+				ret = -1;
+				goto Error1;
+			}
+
+			if (fwrite(buffer, size, 1, fp) < 1)
+			{
+				printf("%s 파일 쓰기 실패\n", filename);
+				ret = -1;
+				goto Error2;
+			}
+
+			printf("%s 파일 추출 성공\n크기: %d\n", filename, size);
+
+		Error2:
+			fclose(fp);
+
+		Error1:
+			free(buffer);
+
+			return ret;
+
+		}
+
+		curr = curr->next;
+	}
+
+	return -1;
+}
+
 int main()
 {
 	PARCHIVE archive =malloc(sizeof(ARCHIVE));
@@ -195,7 +252,8 @@ int main()
 
 	}
 
-	list(archive);
+	//list(archive);
+	extract(archove,"hello.txt");
 
 FINALIZE:
 	curr = archive->fileList.next;
